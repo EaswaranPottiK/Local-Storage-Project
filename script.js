3d792a90864d776c720ce7d3b1f7e0b9efc8eacf
Easwaran <easwaranpottiak@gmail.com>
2023-11-20 23:36:50 +0530

    added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let texta = document.getElementById('texta')
+texta.addEventListener('input', addTOLocalStorage)
+
+function addTOLocalStorage(){
+    localStorage.setItem('savedItem',texta.value)
+}
+
+if(localStorage.getItem('savedItem')){
+    texta.value = localStorage.getItem('savedItem')
+}
